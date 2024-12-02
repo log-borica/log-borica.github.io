6e47f396fd68dde136fe8ef6256dc53887029e9e
gustavoraamos <gustavo.ramos@logcomex.com>
2024-12-02 10:31:29 -0300

    update dez alerts

--- alerts_companies.js
+++ alerts_companies.js
@@ -2,271 +2,271 @@ const alerts_companies = `[
     {
       "id": 200,
       "name": "AFFINITY LOGISTICA INTERNACIONAL LTDA."
-    }
+    },
     {
       "id": 1940,
       "name": "AKHAVA LOGISTICA & COMEX LTDA"
-    }
+    },
     {
       "id": 112,
       "name": "ANX LOGISTICA INTERNACIONAL E AGENCIAMENTO LTDA"
-    }
+    },
     {
       "id": 171,
       "name": "ASLL - AIRSEA LAND ASSESSORIA E LOGISTICA LTDA"
-    }
+    },
     {
       "id": 5032,
       "name": "AUDAZ GLOBAL LOGISTICA LTDA"
-    }
+    },
     {
       "id": 4431,
       "name": "BDN LOGISTICS LTDA"
-    }
+    },
     {
       "id": 7099,
       "name": "BLUE ROYAL LOGISTICA LTDA"
-    }
+    },
     {
       "id": 70,
       "name": "BOSS SHIPPING LOGISTICA LTDA"
-    }
+    },
     {
       "id": 1059,
       "name": "BRAVO CARGO TRANSPORTE INTERNACIONAL LTDA"
-    }
+    },
     {
       "id": 562,
       "name": "BRAZIL WIND LOGISTICS AGENCIAMENTO INTERNACIONAL DE CARGAS LTDA."
-    }
+    },
     {
       "id": 730,
       "name": "CARGOFAST LOGISTICS DO BRASIL LTDA."
-    }
+    },
     {
       "id": 848,
       "name": "CERKLOG LOGISTICA E SERVICOS INTERNACIONAIS LTDA"
-    }
+    },
     {
       "id": 1319,
       "name": "C-FREIGHT AGENCIAMENTOS INTERNACIONAIS LTDA"
-    }
+    },
     {
       "id": 315,
       "name": "COMISSARIA PIBERNAT LTDA"
-    }
+    },
     {
       "id": 8961,
       "name": "CONNECTION CARGO LOGISTICA INTERNACIONAL LTDA"
-    }
+    },
     {
       "id": 3627,
       "name": "CRONOS LOGISTICA LTDA"
-    }
+    },
     {
       "id": 1899,
       "name": "CURTIS LOGISTICA INTERNACIONAL LTDA"
-    }
+    },
     {
       "id": 1993,
       "name": "DUX AGENCIAMENTO DE CARGAS LTDA."
-    }
+    },
     {
       "id": 196,
       "name": "DZL LOGISTICA INTERNACIONAL LTDA"
-    }
+    },
     {
       "id": 554,
       "name": "ETHIMA COMERCIO EXTERIOR LTDA"
-    }
+    },
     {
       "id": 304,
       "name": "EXPEDITORS INTERNATIONAL DO BRASIL LTDA"
-    }
+    },
     {
       "id": 5754,
       "name": "FISCHER & RECHSTEINER DO BRASIL LOGISTICA S.A"
-    }
+    },
     {
       "id": 116,
       "name": "FOX CARGO DO BRASIL LTDA"
-    }
+    },
     {
       "id": 1135,
       "name": "FRAKT LOGISTICS TRANSPORTES INTERNACIONAIS LTDA"
-    }
+    },
     {
       "id": 2101,
       "name": "G4 CARGO DO BRASIL LOGISTICA LTDA"
-    }
+    },
     {
       "id": 1185,
       "name": "GP CARGO AGENCIAMENTO DE CARGAS LTDA"
-    }
+    },
     {
       "id": 1038,
       "name": "GRACO LOGISTICA E TRANSPORTES LTDA"
-    }
+    },
     {
       "id": 318,
       "name": "GWL LOGISTICS IMPORTACAO EXPORTACAO E TRANSPORTES INTERNACIONAIS LTDA"
-    }
+    },
     {
       "id": 1042,
       "name": "HECTOR EXPRESS TRANSPORTATION (BRAZIL) LTDA"
-    }
+    },
     {
       "id": 218,
       "name": "H.M. WAY COMERCIO EXTERIOR LTDA"
-    }
+    },
     {
       "id": 6373,
       "name": "HOFFEN INTERNATIONAL LOGISTICS LTDA"
-    }
+    },
     {
       "id": 1800,
       "name": "IB FREIGHT IMPORTACAO E EXPORTACAO LTDA"
-    }
+    },
     {
       "id": 121,
       "name": "INTERFREIGHT TRANSPORTES INTERNACIONAIS LTDA"
-    }
+    },
     {
       "id": 507,
       "name": "INTERGLOBO DO BRASIL - LOGISTICA INTERNACIONAL LTDA"
-    }
+    },
     {
       "id": 1959,
       "name": "INTERX LOGISTICS LTDA."
-    }
+    },
     {
       "id": 5033,
       "name": "INVICTA COMERCIO EXTERIOR E LOGISTICA LTDA."
-    }
+    },
     {
       "id": 1186,
       "name": "JM LOGISTICA INTERNACIONAL E DESPACHOS ADUANEIROS LTDA."
-    }
+    },
     {
       "id": 3558,
       "name": "KRG LOGISTICA LTDA"
-    }
+    },
     {
       "id": 1013,
       "name": "KSL BR LTDA"
-    }
+    },
     {
       "id": 1861,
       "name": "LAC WORLDWIDE DO BRASIL TRANSPORTES INTERNACIONAIS S.A."
-    }
+    },
     {
       "id": 881,
       "name": "LOCKSLEY LOGISTICA LTDA"
-    }
+    },
     {
       "id": 800,
       "name": "LOGISTIC CO. INTERNACIONAL LTDA"
-    }
+    },
     {
       "id": 95,
       "name": "MASTER SUL COMEX LTDA"
-    }
+    },
     {
       "id": 2617,
       "name": "MAXIMUNDI IMPORTACAO E EXPORTACAO LTDA"
-    }
+    },
     {
       "id": 7324,
       "name": "MPG LOGISTICS BRASIL LTDA"
-    }
+    },
     {
       "id": 769,
       "name": "MULTIDEIAS - MARKETING E TRANSPORTES INTERNACIONAIS LTDA"
-    }
+    },
     {
       "id": 10142,
       "name": "NOMAD AGENCIAMENTO DE CARGAS LTDA"
-    }
+    },
     {
       "id": 1764,
       "name": "NWS LOGISTICA INTERNACIONAL LTDA"
-    }
+    },
     {
       "id": 10845,
       "name": "OXEN CARGO LOGISTICA LTDA"
-    }
+    },
     {
       "id": 772,
       "name": "PARTNER AGENCIAMENTO DE CARGAS LTDA"
-    }
+    },
     {
       "id": 5031,
       "name": "PORT TRADE LOGISTICS LTDA"
-    }
+    },
     {
       "id": 928,
       "name": "PROLIN - IMPORTACAO E EXPORTACAO, PROJETOS E LOGISTICA INTEGRADA LTDA"
-    }
+    },
     {
       "id": 127,
       "name": "QUANTUM LOGISTICA LTDA"
-    }
+    },
     {
       "id": 10676,
       "name": "SATHYA LOGISTICA INTERNACIONAL LTDA"
-    }
+    },
     {
       "id": 422,
       "name": "SCHRYVER DO BRASIL AGENCIAMENTO DE CARGAS LTDA"
-    }
+    },
     {
       "id": 1174,
       "name": "SEA EXPRESS LOGISTICA LTDA"
-    }
+    },
     {
       "id": 9064,
       "name": "STAR BRASIL SERVICOS LOGISTICOS LTDA"
-    }
+    },
     {
       "id": 10143,
       "name": "TEX SHIPPING LTDA"
-    }
+    },
     {
       "id": 188,
       "name": "TRANSIT BR LOGISTICA LTDA"
-    }
+    },
     {
       "id": 674,
       "name": "UW DO BRASIL AGENCIAMENTO DE TRANSPORTES INTERNACIONAIS LTDA."
-    }
+    },
     {
       "id": 295,
       "name": "VENTANA SERRA DO BRASIL AGENCIAMENTO DE CARGAS LTDA"
-    }
+    },
     {
       "id": 66,
       "name": "VIGUIMA LOGISTICS INTERNATIONAL LTDA"
-    }
+    },
     {
       "id": 1883,
       "name": "VIP LOGISTICS BR AGENCIAMENTO E TRANSPORTES LTDA"
-    }
+    },
     {
       "id": 3817,
       "name": "VOX SHIPPING DO BRASIL AGENCIAMENTO LTDA"
-    }
+    },
     {
       "id": 361,
       "name": "V.SANTOS ASSESSORIA ADUANEIRA LTDA."
-    }
+    },
     {
       "id": 2659,
       "name": "WEEDO LOGISTICA BRASIL LTDA"
-    }
+    },
     {
       "id": 10661,
       "name": "WIDE LOGISTICS LTDA"
-    }
+    },
     {
       "id": 6429,
       "name": "WORLD CARGO - LOGISTICA INTERNACIONAL LTDA"
